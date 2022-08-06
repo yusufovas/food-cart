9b493473f336fcdfa5b819dd96323d057d760ad3
yusufovas <maftunayusupova8@gmail.com>
2022-08-06 16:08:43 +0500

    '..'

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,7 +4,6 @@ const menu = require('../controllers/menu')
 const orders = require('../controllers/orders')
 const main = require('../controllers/main')
 const food = require('../controllers/food')
-const cart = require("../controllers/cart")
 
 const router = express.Router()
 
@@ -13,6 +12,5 @@ router
     .get('/menu', menu.get)
     .post('/orders', orders.post)
     .get('/food', food.get)
-    .get('/cart', cart.get)
 
 module.exports = router
